723644af2c903113e8571a659b7d6663d8037469
Samuel Macleod <smacleod@cloudflare.com>
2024-04-18 23:20:03 +0100

    real

--- vitest.shared.ts
+++ vitest.shared.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 		},
 		restoreMocks: true,
 		// A lot of the fixture tests are extremely flaky because of the dev registry
-		// Retry tests by default, so that only realy errors are reported
+		// Retry tests by default so that only real errors are reported
 		retry: 2,
 	},
 });
